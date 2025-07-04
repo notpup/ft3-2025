339fa6e2a791a322e175147ae6a33e13731018ac
Pablo <pablo_98_18@hotmail.com>
2025-07-04 18:19:48 -0300

    Custom error amigo, ya lo necesitaba...

---
+++ src/helpers/customError.js
@@ -0,0 +1,9 @@
+// Gracias felipe x2
+export default class CustomError extends Error {
+  constructor(statusCode, message, details) {
+    super((message = message || "Datos invalidos [Revisar customError.js]"));
+    (this.statusCode = statusCode),
+      (this.details = { success: false, ...details });
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
