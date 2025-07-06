e075e5c13c42ffce5b2eb371b42cc253214dff81
Pablo <pablo_98_18@hotmail.com>
2025-07-06 06:54:46 -0300

    se añadieron algunas funciones para ir testeando solicitudes

--- src/controllers/community.controller.js
+++ src/controllers/community.controller.js
@@ -1,19 +1,87 @@
-//import communityService from "../services/community.service.js";
+import CustomError from "../helpers/customError.js";
+import CommunityService from "../services/community.service.js";
 
 const test = (req, res, next) => {
-	try {
-		return res.status(200).json({
-			message: "lo hiciste bien...",
-			status: 200
-		})
-	} catch(err) {
-		return res.status(500).json({
-			message: "lo hiciste mal, nose como pero lo lograste hacer mal...",
-			status: 500
-		})
-	}
+  try {
+    return res.status(200).json({
+      message: "lo hiciste bien...",
+      status: 200,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: "lo hiciste mal, nose como pero lo lograste hacer mal...",
+      status: 500,
+    });
+  }
+};
+
+const GetUpload = async (req, res, next) => {
+  try {
+    const { q } = req.query
+    const result = await CommunityService.GetUpload({ id: q })
+    return res.status(200).json({
+      success: true,
+      status: 200,
+      response: result
+    });
+  } catch (err) {
+    console.log(err)
+    const statusCode = err.statusCode||500
+    return res.status(statusCode).json(err.details || {
+      success: false,
+      status: statusCode
+    })
+  }
+};
+
+const GetUserUploads = async (req, res, next) => {
+  try {
+    const { page = 1, limit = 25, offset = 0 } = req.query
+    const { userId } = req.params
+    const uploads = await CommunityService.GetUserUploads({ userId, page, limit, offset })
+    return res.status(200).json({
+      success: true,
+      status: 200,
+      response: uploads
+    })
+  } catch (err) {
+    console.log(err)
+    const statusCode = err.statusCode||500
+    return res.status(statusCode).json(err.details || {
+      success: false,
+      status: statusCode
+    })
+  }
 }
 
-export default {
-	test
+const GetUploadsByType = async (req, res, next) => {
+try {
+    const { page = 1, limit = 25, offset = 0 } = req.query
+    const { type } = req.params
+    const uploads = await CommunityService.GetUploadsByType({ type, page, limit, offset })
+    return res.status(200).json({
+      success: true,
+      status: 200,
+      response: uploads
+    })
+  } catch (err) {
+    console.log(err)
+    const statusCode = err.statusCode||500
+    return res.status(statusCode).json(err.details || {
+      success: false,
+      status: statusCode
+    })
+  }
 }
+
+const CreateUpload = (req, res, next) => {};
+
+const CommunityController = {
+  GetUpload,
+  GetUserUploads,
+  GetUploadsByType,
+  CreateUpload,
+  test,
+};
+
+export default CommunityController;
