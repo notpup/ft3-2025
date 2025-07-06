705407628dbb00ffb69353bc5ae3cf207ccee24a
Pablo <pablo_98_18@hotmail.com>
2025-07-06 07:13:27 -0300

    se actualizo un endpoint y se añadio otro

--- src/routers/api/community.routes.js
+++ src/routers/api/community.routes.js
@@ -5,9 +5,10 @@ import middlewares from "../../middlewares/index.js";
 
 const router = express.Router();
 
-router.get("/upload", middlewares.verifyAuthorization, communityController.GetUpload)
+router.get("/upload/:id", middlewares.verifyAuthorization, communityController.GetUpload)
 router.get("/upload/u/:userId", middlewares.verifyAuthorization, communityController.GetUserUploads) 
 router.get("/upload/t/:type", middlewares.verifyAuthorization, communityController.GetUploadsByType) 
+router.get("/upload", middlewares.verifyAuthorization, communityController.GetUploadsByName)
 
 router.post("/upload", middlewares.verifyAuthorization, communityController.CreateUpload)
 
