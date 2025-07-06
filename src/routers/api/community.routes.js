5c2a12e8f5c41613002051db82fdd01db623aa1a
Pablo <pablo_98_18@hotmail.com>
2025-07-06 06:55:28 -0300

    se añadieron algunas apis para ir testeando endpoints

--- src/routers/api/community.routes.js
+++ src/routers/api/community.routes.js
@@ -1,8 +1,14 @@
 import express from "express";
 
 import communityController from "../../controllers/community.controller.js";
+import middlewares from "../../middlewares/index.js";
 
 const router = express.Router();
 
-router.get("/test", communityController.test)
+router.get("/upload", middlewares.verifyAuthorization, communityController.GetUpload)
+router.get("/upload/u/:userId", middlewares.verifyAuthorization, communityController.GetUserUploads) 
+router.get("/upload/t/:type", middlewares.verifyAuthorization, communityController.GetUploadsByType) 
+
+router.post("/upload", middlewares.verifyAuthorization, communityController.CreateUpload)
+
 export default router
