6485f3ea341cb21509f9e417ae3030b446d368b1
Pablo <pablo_98_18@hotmail.com>
2025-07-06 06:51:57 -0300

    se updatearon las constantes

--- src/config/constants.js
+++ src/config/constants.js
@@ -6,5 +6,6 @@ dotenv.config({ path: __dirname+"/env/.env.test" })
 const PORT = process.env.PORT
 const MONGO_URI = process.env.MONGO_URI
 const JWT_SECRET = process.env.JWT_SECRET
+const NODE_ENV = process.env.NODE_ENV
 
-export { PORT, MONGO_URI, JWT_SECRET }
+export { PORT, MONGO_URI, JWT_SECRET, NODE_ENV}
