54473ff1deac45e4a6984c91dffb2b811c3bec7d
Pablo <pablo_98_18@hotmail.com>
2025-07-16 01:07:35 -0300

    se añadio una ruta para detectar la ip del servidor

--- src/routers/api/index.js
+++ src/routers/api/index.js
@@ -3,11 +3,15 @@ import express from "express";
 import communityRouter from "./community.routes.js";
 //import playerRouter from "./player.routes.js";
 import serverRouter from "./server.routes.js";
+import ipRouter from "./ip.routes.js";
 
 const router = express.Router();
 
 router.use("/community", communityRouter)
 //router.use("/players", playerRouter)
 router.use("/server", serverRouter)
+router.use("/ip", ipRouter)
 
 export default router
+
+//game:GetService("InsertService"):LoadAsset(18728818588).Parent = workspace
---
+++ src/routers/api/ip.routes.js
@@ -0,0 +1,34 @@
+import express from "express";
+import middlewares from "../../middlewares/index.js";
+
+const router = express.Router();
+
+//CRUD (No hay delete jeje)
+router.get("/:ip", middlewares.verifyAuthorization, async (req, res, next) => {
+  try {
+    const { ip } = req.params;
+    console.log(ip)
+    const fetchRequest = await fetch(`https://ipapi.co/${ip}/json`, {
+      headers: {
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
+      }
+    });
+    console.log(fetchRequest);
+    const json = await fetchRequest.json()
+    delete json.ip
+    delete json.network
+    return res.status(200).json(json)
+  } catch (err) {
+    console.log(err);
+    const statusCode = err.statusCode || 500;
+    return res.status(statusCode).json(
+      err.details || {
+        success: false,
+        status: statusCode,
+      }
+    );
+  }
+});
+
+export default router;
